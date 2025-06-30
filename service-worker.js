017b00fb9e14c7a805ffddca03cf8cc84fe42b71
AlineUwU <132239125+AlineUwU@users.noreply.github.com>
2025-06-29 22:28:26 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'costos-receta-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './styles.css',
-  './script.js',
-  './icon-192.png',
-  './icon-512.png'
+  'CulinaryCreations/',
+  'CulinaryCreations/index.html',
+  'CulinaryCreations/styles.css',
+  'CulinaryCreations/script.js',
+  'CulinaryCreations/icon-192.png',
+  'CulinaryCreations/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -16,10 +16,15 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        // Si es una navegación (HTML), devuelve index.html
+        if (event.request.mode === 'navigate') {
+          return caches.match('/CulinaryCreations/index.html');
+        }
+      });
     })
   );
 });
